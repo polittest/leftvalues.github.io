03766594b690ab59bbef4b3f54ff1e24a1d786b3
xDMich <149896755+xDMich@users.noreply.github.com>
2024-11-03 19:41:36 +0100

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,17 +1,35 @@
 const ideologies = { // eslint-disable-line no-unused-vars
   'Marxism-Leninism': {
     a: 80,
-    b: 80,
-    c: 80,
+    b: 60,
+    c: 100,
     d: 60,
-    e: 90,
+    e: 100,
     f: 70,
     g: 40
+  },
+  'Dengism': {
+    a: 50,
+    b: 50,
+    c: 100,
+    d: 30,
+    e: 100,
+    f: 70,
+    g: 70
+  },
+  'Juche': {
+    a: 20,
+    b: 40,
+    c: 100,
+    d: 0,
+    e: 100,
+    f: 70,
+    g: 80
   },
   'Orthodox-Marxism': {
     a: 80,
     b: 80,
-    c: 60,
+    c: 40,
     d: 80,
     e: 70,
     f: 50,
@@ -37,8 +55,8 @@ const ideologies = { // eslint-disable-line no-unused-vars
   },
   'Council-Communism': {
     a: 100,
-    b: 70,
-    c: 40,
+    b: 80,
+    c: 30,
     d: 80,
     e: 50,
     f: 50,
@@ -46,19 +64,37 @@ const ideologies = { // eslint-disable-line no-unused-vars
   },
   'Left-Communism': {
     a: 90,
-    b: 60,
-    c: 50,
+    b: 100,
+    c: 60,
     d: 90,
     e: 80,
     f: 50,
     g: 30
   },
-  'Anarcho-Communism': {
+  'Anarcho-Syndicalism': {
     a: 80,
-    b: 50,
-    c: 20,
+    b: 70,
+    c: 30,
     d: 80,
-    e: 10,
+    e: 0,
+    f: 50,
+    g: 30
+  },
+  'Platformism': {
+    a: 80,
+    b: 70,
+    c: 25,
+    d: 80,
+    e: 50,
+    f: 50,
+    g: 30
+  },
+  'Insurrectionist anarchism': {
+    a: 80,
+    b: 70,
+    c: 0,
+    d: 80,
+    e: 20,
     f: 50,
     g: 30
   },
@@ -80,15 +116,6 @@ const ideologies = { // eslint-disable-line no-unused-vars
     f: 60,
     g: 30
   },
-  'Utopian-Socialism': {
-    a: 10,
-    b: 10,
-    c: 40,
-    d: 20,
-    e: 50,
-    f: 40,
-    g: 50
-  },
   'Democratic-Socialism': {
     a: 20,
     b: 30,
@@ -108,8 +135,8 @@ const ideologies = { // eslint-disable-line no-unused-vars
     g: 30
   },
   'Left-Wing-Nationalism': {
-    a: 60,
-    b: 60,
+    a: 30,
+    b: 40,
     c: 60,
     d: 30,
     e: 70,
